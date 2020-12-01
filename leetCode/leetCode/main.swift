f73d373c1a31598bc90a31b97e87ebb442ffb9ef
yuanyuntian <yuanf1990@163.com>
2020-12-02 00:44:12 +0800

    添加字符串

--- leetCode/leetCode/main.swift
+++ leetCode/leetCode/main.swift
@@ -10,7 +10,8 @@ import Foundation
 
 var a = [1,10,20,11]
 var b = Array().searchInsert(a, 20)
-
+var str = ["abc", "abdd", "abeed"]
+var c = _String().longestCommonPrefix(str)
 
 print("结果:%d",b)
 
