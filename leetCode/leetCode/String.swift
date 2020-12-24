5367f76aceae2fc2c2395922b981318607344b14
yuanyuntian <yuanf1990@163.com>
2020-12-24 23:42:37 +0800

    双指针

--- leetCode/leetCode/String.swift
+++ leetCode/leetCode/String.swift
@@ -186,5 +186,39 @@ class _String {
         }
         return [-1, -1]
     }
+    
+    /*移除元素
+    给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+    不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+    元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。*/
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        let n = nums.count
+        var slow = 0
+        if n == 0 {
+            return 0
+        }else{
+            for i in 0..<n {
+                if nums[i] != val {
+                    nums[slow] = nums[i]
+                    slow += 1
+                }
+            }
+        }
+        return slow
+    }
+    
+    func removeElement1(_ nums: inout [Int], _ val: Int) -> Int {
+        var n = nums.count
+        var i = 0
+        while i < n {
+            if nums[i] == val {
+                nums[i] = nums[n - 1]
+                n -= 1
+            } else {
+                i += 1
+            }
+        }
+        return n
+    }
 
 }
